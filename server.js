059e5b379b722054df8a2757d7c26ef3f6c94728
Mahesh-Dev23 <nomahesh@gmail.com>
2023-12-25 20:22:32 +0530

    mongoose config added

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+
+const path = require('path')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+
+const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
+const app = express()
+
+ app.use(cors())
+ app.use(express.json())
+ app.use(express.urlencoded({extended:false}))
+
+ app.get('/',(req, res)=> {
+    res.send('Welcome to "Podium-AI-Backend App!')
+    console.log('Welcome to "Podium-AI-Backend App!')
+    })
+
+
+ app.listen(PORT, ()=>{
+    console.log(`This is express server running on port: ${PORT}`)
+    console.log(`This is podium-ai-backend app`)
+    })
+
+connectDB() 
