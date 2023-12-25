c42fac7301b8cc81bf83133dd221163c84e50eaf
Mahesh-Dev23 <nomahesh@gmail.com>
2023-12-25 20:42:08 +0530

    cors added

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const app = express()
 
- app.use(cors())
+ app.use(cors({
+    origin:"*",
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+ }))
  app.use(express.json())
  app.use(express.urlencoded({extended:false}))
 
